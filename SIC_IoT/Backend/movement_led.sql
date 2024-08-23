20e8c757cfdad40a8c48e0e458d8547c22526b3d
Flash <namquan100803@gmail.com>
2024-08-23 10:31:00 +0700

    Fix sql file

--- SIC_IoT/Backend/movement_led.sql
+++ SIC_IoT/Backend/movement_led.sql
@@ -8,11 +8,11 @@ USE energy_management;
 -- Drop table energy_usage_daily;
 -- Drop table energy_usage_weekly;
 
-CREATE TABLE energy_consumed (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    date date,
-    e_usage float
-);
+-- CREATE TABLE energy_consumed (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     date date,
+--     e_usage float
+-- );
 
 CREATE TABLE energy_usage_daily (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -20,11 +20,11 @@ CREATE TABLE energy_usage_daily (
     e_usage_daily float unique key 
 );
 
-CREATE TABLE energy_usage_weekly (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    date date,
-    e_usage_weekly float
-);
+-- CREATE TABLE energy_usage_weekly (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     date date,
+--     e_usage_weekly float
+-- );
 
 CREATE TABLE energy_usage_monthly (
     id INT AUTO_INCREMENT PRIMARY KEY,
